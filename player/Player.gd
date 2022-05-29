c86ee799564f4f188a9d1dfb254aa84d8cc54840
Dardo Marasca <dardo.marasca@jampp.com>
2022-05-29 20:21:21 -0300

    Remove jump and gravity

--- player/Player.gd
+++ player/Player.gd
@@ -3,8 +3,7 @@ extends KinematicBody2D
 var velocity = Vector2.ZERO
 
 export (int) var speed = 100
-export (int) var jump_speed = -200
-export (int) var gravity = 500
+
 
 func _physics_process(delta):
 
@@ -21,12 +20,5 @@ func _physics_process(delta):
 		$AnimatedSprite.animation = "idle"
 	else:
 		$AnimatedSprite.animation = "walk"
-	
-	velocity.y += gravity * delta
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			velocity.y = jump_speed
-	if not is_on_floor():
-		$AnimatedSprite.animation = "jump"
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
